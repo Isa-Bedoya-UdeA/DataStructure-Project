1895b542e325c6a75cc27be6548327735767a309
mitumial <ace.vortex76@gmail.com>
2025-09-21 23:46:33 -0500

    create form and add theme

---
+++ streamlit_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+def create_character(name, class_rpg, race, skills):
+    character = {
+        "name": name,
+        "class": class_rpg,
+        "race": race,
+        "skills": skills
+    }
+    return character
+
+with st.form("character_form", clear_on_submit=True):
+    st.header("Creador de Personaje RPG")
+    
+    name = st.text_input("Nombre del personaje", placeholder="Ingresa un nombre para tu personaje", max_chars=16)
+    class_rpg = st.selectbox("Clase", ["Guerrero", "Mago", "Clérigo", "Paladín", "Bárbaro", "Asesino", "Druida", "Arquero", "Nigromante", "Monje"], placeholder="Selecciona tu clase")
+    race = st.selectbox("Raza", ["Humano", "Elfo", "Enano", "Orco", "Gnomo", "Centauro", "Ciclope", "Duende", "Sirena"], placeholder="Selecciona tu raza")
+    skills = st.pills("Habilidades", ["Ataque físico", "Ataque mágico", "Sigilo", "Curación", "Guardia", "Invocación", "Esquivar", "Arquería", "Combate cuerpo a cuerpo"], selection_mode="multi") 
+    st.caption("Debes seleccionar 6 habilidades distintas.")
+
+    submitted = st.form_submit_button("Crear personaje") ## , on_click=create_character(name, class_rpg, race, skills)
+    
+    if submitted:
+        st.success('**¡Personaje creado exitosamente!**')
+        st.write(submitted)
